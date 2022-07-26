93e1b5c10bd256a3574e0fea42f3786b67e22e56
Ekaterina Zhirova <mymymyr@yandex.ru>
2022-07-26 20:19:23 +0300

    move close popup to main.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,3 @@
-const buttonClose = document.querySelector ('.popup__button_close');
-const popup = document.querySelector('.popup');
-
-
-function closePopup(popupElement) {    //ФУНКЦИЯ ЗАКРЫТИЯ ПОПАПА
-    popupElement.classList.add('popup_closed');
-  }
-
-buttonClose.addEventListener('click', function (){    //Закрытие попапа на кнопку
-    closePopup(popup)
-});
-
-//-------------------------------------------------------------------------------
-
 const extraMenu = document.querySelector('.header__extra-menu'),
       menuButtonAbout = document.querySelector('#header-menu-about'),
       mobileMenu = document.querySelector('.header__mobile-menu'),
