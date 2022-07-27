47f00abcd7da727b8c2a462a2a0107aeb0df5a98
Ekaterina Zhirova <mymymyr@yandex.ru>
2022-07-27 18:44:30 +0300

    fix some remarks

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,46 @@
-const buttonClose = document.querySelector ('.popup__button_close');
+const buttonClose = document.querySelector('.popup__button_close');
 const popup = document.querySelector('.popup');
 
+const extraMenu = document.querySelector('.header__extra-menu');
+const menuButtonAbout = document.querySelector('#header-menu-about');
+const mobileMenu = document.querySelector('.header__mobile-menu');
+const mobileMenuButton = document.querySelector('.header__menu-button');
 
-function closePopup(popupElement) {    //ФУНКЦИЯ ЗАКРЫТИЯ ПОПАПА
-    popupElement.classList.add('popup_closed');
+
+function closePopup(popupElement) { //ФУНКЦИЯ ЗАКРЫТИЯ ПОПАПА
+  popupElement.classList.add('popup_closed');
+}
+
+function addEventListeners() {
+  if (buttonClose) {
+    buttonClose.addEventListener('click', function () { //Закрытие попапа на кнопку
+      closePopup(popup)
+    });
   }
 
-buttonClose.addEventListener('click', function (){    //Закрытие попапа на кнопку
-    closePopup(popup)
-});
-
-const extraMenu = document.querySelector('.header__extra-menu'),
-      menuButtonAbout = document.querySelector('#header-menu-about'),
-      mobileMenu = document.querySelector('.header__mobile-menu'),
-      mobileMenuButton = document.querySelector('.header__menu-button');
-
-mobileMenuButton.addEventListener("click", function(){
-    if (mobileMenu.classList.contains('header__mobile-menu_active')) {
-       mobileMenu.classList.remove('header__mobile-menu_active');
-       mobileMenuButton.style = "background-image: url('../blocks/header/images/mobile-menu.svg')";
-    } else {
+  if (mobileMenuButton && mobileMenu) {
+    mobileMenuButton.addEventListener("click", function () {
+      if (mobileMenu.classList.contains('header__mobile-menu_active')) {
+        mobileMenu.classList.remove('header__mobile-menu_active');
+        mobileMenuButton.style = "background-image: url('../blocks/header/images/mobile-menu.svg')";
+      } else {
         mobileMenu.classList.add('header__mobile-menu_active');
         mobileMenuButton.style = "background-image: url('../blocks/header/images/close.svg')";
-    }
-});
+      }
+    });
+  }
 
-menuButtonAbout.onmouseover = function(e) {
-    extraMenu.classList.add('header__extra-menu_active');
-};
+  if (menuButtonAbout) {
+    menuButtonAbout.onmouseover = function (e) {
+      extraMenu.classList.add('header__extra-menu_active');
+    };
+  }
 
-extraMenu.addEventListener("mouseleave", function(e){
-    extraMenu.classList.remove('header__extra-menu_active');
-});
+  if (extraMenu) {
+    extraMenu.addEventListener("mouseleave", function (e) {
+      extraMenu.classList.remove('header__extra-menu_active');
+    });
+  }
+}
+
+addEventListeners();
